b887b1aace7be918b7e5568b0df163111f9e729f
hedstromfredrik10-a11y <hedstromfredrik10@gmail.com>
2025-10-23 01:33:40 +0200

    Sort

--- HackerRank_Problems/src/Data_Structures/Java_1D_Array_Part_2_V2.java
+++ HackerRank_Problems/src/Data_Structures/Java_1D_Array_Part_2_V2.java
@@ -1,90 +1,43 @@
 package Data_Structures;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Java_1D_Array_Part_2_V2 {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int q = scan.nextInt();
-        scan.nextLine();
-        int counter = 0;
+    private static boolean check(int i, int leap, int[] arr) {
+        if (i < 0 || arr[i] == 1)
+            return false;
+        if (i + leap >= arr.length || i == arr.length - 1)
+            return true;
+        arr[i] = 1;
 
-        while (counter < q) {
-            int n = scan.nextInt();
-
-            int leap = scan.nextInt();
-            int[] game = new int[n];
-            for (int i = 0; i < n; i++) {
-                game[i] = scan.nextInt();
-            }
-
-            if (canWin.leapTest(game, n, leap)) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
-            }
-            counter++;
-
-        }
-        scan.close();
+        return check(i + leap, leap, arr) ||
+                check(i + 1, leap, arr) ||
+                check(i - 1, leap, arr);
     }
-}
 
-class canWin {
-    public static boolean leapTest(int[] game, int n, int leap) {
-        boolean win = true;
-        boolean flag = true;
-        int x = 0;
-        for (int i = 0; i < game.length; i++) {
-            int failCounter = 0;
-            try {
-                if (game[i] == 0) {
-                    if (game[i + leap] == 0) {
-                        i = i + leap;
-                    } else {
-                        failCounter++;
-                        if (game[i + 1] == 0) {
-                            i = i + 1;
-                            if (game[i + leap] == 0) {
-                                i = i + leap;
-                            } else {
-                                flag = false;
-                            }
-                        } else if (game[i - 1] == 0) {
-                            i = i - 1;
-                            if (game[i + leap] == 0) {
-                                i = i + leap;
-                            } else {
-                                flag = false;
-                            }
-                        }
-                    }
-                } else if (game[i] == 1) {
-                    int onesInARow = 0;
-                    x = i;
-                    while (game[x] == 1) {
-                        onesInARow++;
-                        if (x == (game.length - 1)) {
-                            break;
-                        } else {
-                            x++;
-                        }
-                    }
-                    if (onesInARow >= leap) {
-                        i = game.length;
-                        win = false;
-                    }
-                }
-                if (flag == false) {
-                    win = false;
-                    i = game.length;
-                }
-
-            } catch (Exception e) {
-                win = true;
+    public static void main(String[] args) throws IOException {
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int leap = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
             }
+            System.out.println(Java_1D_Array_Part_2_V2.check(0, leap, arr) ? "YES" : "NO");
+
         }
 
-        return win;
     }
 }
---
+++ HackerRank_Problems/src/Data_Structures/Java_sort.java
@@ -0,0 +1,18 @@
+package Data_Structures;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Java_sort {
+    public static void main(String[] args) {
+        Map<Integer, String> list = new HashMap<>();
+        
+        list.put(15, "Calle");
+        list.put(124, "Roger");
+        list.put(1512, "Karin");
+
+        
+        System.out.println(list.toString());
+        
+    }
+}
