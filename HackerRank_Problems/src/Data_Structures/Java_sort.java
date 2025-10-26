5a3f2d79624aa26e17410c5992553ff1e46090fd
hedstromfredrik10-a11y <hedstromfredrik10@gmail.com>
2025-10-26 22:13:59 +0100

    Comparator_2

--- HackerRank_Problems/src/Data_Structures/Java_sort.java
+++ HackerRank_Problems/src/Data_Structures/Java_sort.java
@@ -1,14 +1,9 @@
 package Data_Structures;
 
-import java.lang.reflect.Array;
-import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Java_sort {
@@ -18,14 +13,14 @@ public class Java_sort {
         int count = Integer.parseInt(scan.nextLine());
         int counter = 0;
         String[] values = new String[3];
-        List<Student> stud = new ArrayList<>();
+        List<Student> studList = new ArrayList<>();
 
         while (counter < count) {
             String input = scan.nextLine();
 
             values = input.split("\\s+");
 
-            stud.add(new Student(Integer.parseInt(values[0]), values[1], Double.parseDouble(values[2])));
+            studList.add(new Student(Integer.parseInt(values[0]), values[1], Double.parseDouble(values[2])));
 
             for (int i = 0; i < values.length - 1; i++) {
                 values[i] = "\b";
@@ -34,24 +29,21 @@ public class Java_sort {
             counter++;
         }
 
-        Comparator<Student> com = (Student i, Student j) -> {
-            if (i.cgpa > i.cgpa) {
-                return 1;
-
+        Comparator<Student> com = (Student a, Student b) -> {
+            if (!a.cgpa.equals(b.cgpa)) {
+                return b.cgpa.compareTo(a.cgpa);
+            } else if (!a.name.equals(b.name)) {
+                return a.name.compareTo(b.name);
             } else {
-                return -1;
+                return Integer.compare(a.id, b.id);
             }
-
         };
 
-        for (Student s : stud) {
-
-        }
-
-        Collections.sort(stud, com);
+        Collections.sort(studList, com);
         System.out.println();
-        for (Student s : stud) {
-            System.out.println(s);
+
+        for (Student s : studList) {
+            System.out.println(s.getName());
         }
         scan.close();
 
@@ -59,42 +51,34 @@ public class Java_sort {
 
     public static class Student {
 
-        int id;
-        String name;
-        Double cgpa;
+        private int id;
+        private String name;
+        private Double cgpa;
 
         public Student(int id, String name, double cgpa) {
             this.id = id;
             this.name = name;
             this.cgpa = cgpa;
 
-            setID(id);
-            setName(name);
-            setCGPA(cgpa);
-
-        }
-
-        public void setID(int id) {
             List<Integer> ID = new ArrayList<>();
-            ID.add(id);
-        }
-
-        public void setName(String name) {
             List<String> NAME = new ArrayList<>();
-            NAME.add(name);
-        }
-
-        public void setCGPA(double cgpa) {
             List<Double> CGPA = new ArrayList<>();
-            CGPA.add(cgpa);
-        }
 
-        public String toString() {
-            return id + " " + name + " " + cgpa;
+            ID.add(id);
+            NAME.add(name);
+            CGPA.add(cgpa);
         }
 
         public double getCGPA() {
             return cgpa;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getID() {
+            return id;
         }
     }
 
