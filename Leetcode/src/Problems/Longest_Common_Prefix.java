c0478b2c4231a4a697f5c8659b724992c18aac5d
hedstromfredrik10-a11y <hedstromfredrik10@gmail.com>
2025-11-07 20:08:41 +0100

    Shitty formating

--- Leetcode/src/Problems/Longest_Common_Prefix.java
+++ Leetcode/src/Problems/Longest_Common_Prefix.java
@@ -36,29 +36,11 @@ public class Longest_Common_Prefix {
             for (int i = 0; i < strs[0].length(); i++) {
                 String check = String.valueOf(strs[0].charAt(i));
                 for (int x = 0; x < strs.length; x++) {
-                    try {
-                        if (check.equals(String.valueOf(strs[x].charAt(i)))) {
-                            flag = true;
-                        } else {
-                            flag = false;
-                            x = strs.length;
-                            i = strs[0].length();
-                        }
-                    } catch (Exception e) {
-                        x = strs.length;
-                        flag = false;
-                    }
-
-                }
-                if (flag) {
-                    s.append(check);
-                }
-            }
-            if (s.length() > 0) {
-                return s.toString();
-            } else {
-                return "";
-            }
+                    try {if (check.equals(String.valueOf(strs[x].charAt(i)))) {flag = true;
+                        } else {flag = false; x = strs.length; i = strs[0].length();}
+                    } catch (Exception e) {x = strs.length; flag = false;}}
+                if (flag) {s.append(check);}}
+            if (s.length() > 0) {return s.toString();} else {return "";}
         }
 
     }
