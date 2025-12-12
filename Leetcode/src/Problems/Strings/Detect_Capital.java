bb520060376bbbfc8d0cda537a8b888a44a87c44
hedstromfredrik10-a11y <hedstromfredrik10@gmail.com>
2025-12-12 18:14:05 +0100

    detect capital fin

--- Leetcode/src/Problems/Strings/Detect_Capital.java
+++ Leetcode/src/Problems/Strings/Detect_Capital.java
@@ -17,15 +17,63 @@ package Problems.Strings;
 
 public class Detect_Capital {
     public static void main(String[] args) {
-        String word = "USA";
+        // String word = "USA";
+        String word = "Leetcode";
         System.out.println(detectCapitalUse(word));
     }
 
     public static boolean detectCapitalUse(String word) {
         boolean result = false;
-        
+
+        if (word.length() == 1) {
+            return true;
+        }
+        char[] arr = word.toCharArray();
+        if (Character.isUpperCase(arr[0])) {
+            if (checkAllUpperCase(arr) == true && checkAllLowerCase(arr) == false) {
+                result = true;
+            }
+        }
+
+        if (Character.isUpperCase(arr[0])) {
+            if (checkAllUpperCase(arr) == false && checkAllLowerCase(arr) == true) {
+                result = true;
+            }
+        }
+
+        if (Character.isLowerCase(arr[0])) {
+            if (checkAllLowerCase(arr)) {
+                result = true;
+            }
+        }
 
         return result;
 
     }
+
+    public static boolean checkAllUpperCase(char[] arr) {
+        boolean result = true;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (Character.isLowerCase(arr[i])) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean checkAllLowerCase(char[] arr) {
+        boolean result = true;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (Character.isUpperCase(arr[i])) {
+                result = false;
+                break;
+            }
+
+        }
+        return result;
+    }
+
 }
