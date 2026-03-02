312497f841c214ff2a53e9f8283c7b61b41bd7b9
hedstromfredrik10-a11y <hedstromfredrik10@gmail.com>
2026-03-02 21:12:23 +0100

    pass 2

--- Tentor/src/PasswordCreater.java
+++ Tentor/src/PasswordCreater.java
@@ -21,33 +21,35 @@ public class PasswordCreater {
     private static final String SPECIAL_CHARACTERS = "-_!?";
 
     public static void main(String[] args) {
-        int length = 10;
+        int length = 14;
         System.out.println(passwordCreator(length));
     }
 
     public static String passwordCreator(int length) {
         String[] values = { LOWERCASE, UPPERCASE, DIGITS, SPECIAL_CHARACTERS };
-        List<Character> result = new ArrayList<>();
+        List<Character> list = new ArrayList<>();
         if (length < 4) {
             throw new IllegalArgumentException("Password length to short");
         }
         StringBuilder password = new StringBuilder();
         Random gen = new Random();
 
-        char lowercase = LOWERCASE.charAt(gen.nextInt(LOWERCASE.length() - 1)),
-                uppercase = UPPERCASE.charAt(gen.nextInt(UPPERCASE.length() - 1)),
-                digits = DIGITS.charAt(gen.nextInt(DIGITS.length() - 1)),
-                specialCharacter = SPECIAL_CHARACTERS.charAt(gen.nextInt(SPECIAL_CHARACTERS.length() - 1));
-
-        password.append(lowercase).append(uppercase).append(digits).append(specialCharacter);
+        list.add(LOWERCASE.charAt(gen.nextInt(LOWERCASE.length() - 1)));
+        list.add(UPPERCASE.charAt(gen.nextInt(UPPERCASE.length() - 1)));
+        list.add(DIGITS.charAt(gen.nextInt(DIGITS.length() - 1)));
+        list.add(SPECIAL_CHARACTERS.charAt(gen.nextInt(SPECIAL_CHARACTERS.length() - 1)));
 
         for (int i = 0; i < length - 4; i++) {
             String selectedValue = values[gen.nextInt(4)];
             char c = selectedValue.charAt(gen.nextInt(selectedValue.length()));
-            password.append(c);
+            list.add(c);
         }
 
-        // System.out.println(password.toString());
+        Collections.shuffle(list);
+
+        for (Character character : list) {
+            password.append(character);
+        }
 
         return password.toString();
     }
